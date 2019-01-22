f90ba0e9918164e6ec74e6098530ceb6109a782a
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-01-22 15:17:45 -0500

    changesfor app.js

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -6,19 +6,10 @@ import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.friends to the friends json array
   state = {
     cards
   };
 
-//   removeFriend = id => {
-//     // Filter this.state.friends for friends with an id not equal to the id being removed
-//     const friends = this.state.friends.filter(friend => friend.id !== id);
-//     // Set this.state.friends equal to the new friends array
-//     this.setState({ friends });
-//   };
-
-  // Map over this.state.friends and render a CharCard component for each friend object
   render() {
     return (
       <Wrapper>
