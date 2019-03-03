0d7a93d5eb982333dbda99fac6ffdd499af181f7
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-03-03 14:52:19 -0500

    Update Jumbotron.js

--- reactpractice/src/components/Jumbotron/Jumbotron.js
+++ reactpractice/src/components/Jumbotron/Jumbotron.js
@@ -4,7 +4,7 @@ import "./Jumbotron.css";
 const Jumbotron = () => (
 <header className="Jumbotron">
     <h1>Clicky Game!</h1>
-    <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+    <h2>Click on an image to earn points, but don not click on any more than once!</h2>
 </header>
 )
 
