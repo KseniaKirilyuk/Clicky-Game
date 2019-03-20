7286b2afc0f8ffe868df6d39dff5e7238e6bca6c
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-03-20 15:55:12 -0400

    Updates

---
+++ precache-manifest.f6da62982d176826a2e5c220c043c220.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "4505bc37547c2c16574f",
+    "url": "/Clicky-Game/static/js/main.4505bc37.chunk.js"
+  },
+  {
+    "revision": "ab9342f10f77e96e015d",
+    "url": "/Clicky-Game/static/js/1.ab9342f1.chunk.js"
+  },
+  {
+    "revision": "4505bc37547c2c16574f",
+    "url": "/Clicky-Game/static/css/main.48932473.chunk.css"
+  },
+  {
+    "revision": "52c20931342138727deda0fcf7cb8c39",
+    "url": "/Clicky-Game/index.html"
+  }
+];
