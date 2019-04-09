66029df76e442f3f12cb9ec4a96895c16ba6e4f0
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-04-09 18:34:04 -0400

    final style

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -5,6 +5,7 @@ const Jumbotron = () => (
 <header className="Jumbotron">
     <h1>Clicky Game!</h1>
     <h2>Click on an image to earn points, but don not click on any more than once!</h2>
+    <h3><i>Enjoy amazing artpieces</i></h3>
 </header>
 )
 
